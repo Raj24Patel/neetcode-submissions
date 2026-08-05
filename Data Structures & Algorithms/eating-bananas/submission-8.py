80097ca5e117c2cccdf62bfd063958baabb22db0
Raj Patel <rajhpatel2022@gmail.com>
2026-08-04 22:02:02 -0400

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        least = r = max(piles)
+        
+        count = 0
+
+        while l <= r:
+            k = l + (r - l) // 2
+
+            for bananas in piles:
+                x = math.ceil(bananas/k) 
+                count += x
+            
+            if count <= h:
+                least = min(least, k)
+                r = k - 1
+            else: 
+                l = k + 1
+
+            count = 0
+        
+        return least
+            
+
+
+            
